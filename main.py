57a94db23e44e596a263a747888115d58a6fd5ac
Louis Jalouzot <ljalouzo@margaret.saclay.inria.fr>
2024-06-17 14:47:12 +0200

    Argparse

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 #!.env/bin/python
 from src.train import train
+import argparse
 
-train(
-    subject="UTS03",
-    model="sentence-transformers/all-mpnet-base-v2",
-    # model="mel",
-    decoder="brain_decoder",
-    context_length=4,
-    lag=2,
-    smooth=0,
-    subsample_voxels=10_000,
-    hidden_size_backbone=512,
-    hidden_size_projector=512,
-    dropout=0.7,
-    n_res_blocks=2,
-    n_proj_blocks=1,
-    lr=1e-4,
-    weight_decay=1e-6,
-    batch_size=1024,
-    temperature=0.01,
-)
+parser = argparse.ArgumentParser(description="Argument parser")
+parser.add_argument("--subject", type=str, default="UTS03", help="Subject name")
+parser.add_argument("--model", type=str, default="sentence-transformers/all-mpnet-base-v2", help="Model name")
+parser.add_argument("--decoder", type=str, default="brain_decoder", help="Decoder name")
+parser.add_argument("--context_length", type=int, default=4, help="Context length")
+parser.add_argument("--lag", type=int, default=2, help="Lag")
+parser.add_argument("--smooth", type=int, default=1, help="Smooth")
+parser.add_argument("--subsample_voxels", type=int, default=None, help="Number of voxels to subsample")
+parser.add_argument("--hidden_size_backbone", type=int, default=512, help="Hidden size for backbone")
+parser.add_argument("--hidden_size_projector", type=int, default=512, help="Hidden size for projector")
+parser.add_argument("--dropout", type=float, default=0.7, help="Dropout rate")
+parser.add_argument("--n_res_blocks", type=int, default=2, help="Number of residual blocks")
+parser.add_argument("--n_proj_blocks", type=int, default=1, help="Number of projection blocks")
+parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate")
+parser.add_argument("--weight_decay", type=float, default=1e-6, help="Weight decay")
+parser.add_argument("--batch_size", type=int, default=1024, help="Batch size")
+parser.add_argument("--temperature", type=float, default=0.01, help="Temperature")
+
+args = parser.parse_args()
+
+train(**vars(args))

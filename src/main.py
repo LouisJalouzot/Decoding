bdb3cb796c99c4d7544f750bc996fc47c02c3a0d
Louis Jalouzot <86967472+LouisJalouzot@users.noreply.github.com>
2024-07-18 10:35:07 +0200

    Minor

--- src/main.py
+++ src/main.py
@@ -7,9 +7,10 @@ from src.train import train
 from src.utils import ignore, memory
 
 
-# @memory.cache
+@memory.cache
 def main(
     datasets: Union[str, List[str]] = "lebel2023/all_subjects",
+    subjects: List[str] = None,
     decoder: str = "brain_decoder",
     model: str = "bert-base-uncased",
     context_length: int = 6,
@@ -19,12 +20,16 @@ def main(
     multi_subject_mode: str = "individual",
     **kwargs,
 ):
-    if isinstance(datasets, str):
-        subjects = [f"{datasets}/{f}" for f in os.listdir(f"data/{datasets}")]
-    elif isinstance(datasets, list):
-        subjects = []
-        for dataset in datasets:
-            subjects.extend([f"{dataset}/{f}" for f in os.listdir(f"data/{dataset}")])
+    if subjects is None:
+        if isinstance(datasets, str):
+            subjects = [f"{datasets}/{f}" for f in os.listdir(f"data/{datasets}")]
+        elif isinstance(datasets, list):
+            subjects = []
+            for dataset in datasets:
+                subjects.extend(
+                    [f"{dataset}/{f}" for f in os.listdir(f"data/{dataset}")]
+                )
+
     config = {
         key: value
         for key, value in locals().items()
@@ -34,7 +39,7 @@ def main(
     wandb.init(
         config=config,
         id=sha1(repr(sorted(config.items())).encode()).hexdigest(),
-        project="fMRI-Decoding",
+        project="fMRI-Decoding-v3",
         save_code=True,
     )
     output = train(**config)

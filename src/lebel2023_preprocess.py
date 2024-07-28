e713ac0b8110907861245efca0857e3f14764124
Louis Jalouzot <86967472+LouisJalouzot@users.noreply.github.com>
2024-07-28 18:51:04 +0200

    netCDF

--- src/lebel2023_preprocess.py
+++ src/lebel2023_preprocess.py
@@ -1,23 +1,23 @@
 import shutil
 from pathlib import Path
 
-import h5py
 import numpy as np
 import xarray as xr
 from dask.diagnostics import ProgressBar
 from joblib import Parallel, delayed
 from joblib_progress import joblib_progress
 
-from src.utils import standard_scale
+
+def standard_scale(ds: xr.DataArray, along=["run_id", "tr"]):
+    ds_mean = ds.mean(dim=along, skipna=True)
+    ds_scale = ds.fillna(0).std(dim=along, skipna=True)
+    ds_scale = xr.where(ds_scale < 1e-6, 1, ds_scale)
+    return (ds - ds_mean) / ds_scale
 
 
 def read(subject, run_name, run):
-    with h5py.File(run, "r") as f:
-        a = xr.DataArray(f["data"][...], dims=["tr", "voxel"]).astype(np.float32)
-    a = a.fillna(0)
-    a_mean = a.mean(dim="tr")
-    a_std = a.std(dim="tr")
-    a = (a - a_mean) / xr.where(a_std < 1e-6, 1, a_std)
+    a = xr.open_dataarray(run).astype(np.float32)
+    a = a.rename({"phony_dim_0": "tr", "phony_dim_1": "voxel"})
     a = a.expand_dims(dim="run_id", axis=0)
     a["run_id"] = [f"lebel2023/{subject}/{run_name}"]
     a = a.assign_coords(
@@ -29,18 +29,23 @@ def read(subject, run_name, run):
         tr=np.arange(a.tr.size),
         voxel=np.arange(a.voxel.size),
     )
-    return standard_scale(a, along="tr")
+    return standard_scale(a.fillna(0), along="tr")
 
 
-def create_zarr_dataset(subjects=["UTS01", "UTS02", "UTS03"], name="3_subjects"):
-    dataset_path = Path(f"data/lebel2023/{name}.zarr")
+def create_zarr_dataset(
+    subjects=["UTS01", "UTS02", "UTS03"], name="3_subjects", format="nc"
+):
+    dataset_path = Path(f"data/lebel2023/{name}.{format}")
     if dataset_path.exists():
         if input(
             f"{dataset_path} already exists, do you want to overwrite it? (Y/n)"
         ) not in ["y", ""]:
             return
         else:
-            shutil.rmtree(dataset_path)
+            if dataset_path.is_file():
+                dataset_path.unlink()
+            else:
+                shutil.rmdir(dataset_path)
     path = Path("data/lebel2023/derivative/preprocessed_data")
     runs = {subject: list((path / subject).iterdir()) for subject in subjects}
     with joblib_progress(
@@ -56,4 +61,7 @@ def create_zarr_dataset(subjects=["UTS01", "UTS02", "UTS03"], name="3_subjects")
         ds = xr.concat(ds, dim="run_id")
         ds = ds.chunk({"run_id": 1, "voxel": ds.voxel.size, "tr": ds.tr.size})
         ds_scaled = ds.groupby("subject").map(standard_scale)
-        ds_scaled.to_dataset(name="X").to_zarr(dataset_path)
+        if format == "zarr":
+            ds_scaled.to_dataset(name="data").to_zarr(dataset_path)
+        else:
+            ds_scaled.to_netcdf(dataset_path, engine="netcdf4")

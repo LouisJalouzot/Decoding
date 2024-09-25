ece8e2c93b2a4a6490b1c2f9d4b03517e2f2b965
Louis Jalouzot <86967472+LouisJalouzot@users.noreply.github.com>
2024-09-25 16:43:09 +0200

    Preprocess FR/CN

--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,9 @@
 from src.lebel2023_preprocess import create_lebel2023_dataset
-from src.li2022_preprocess import (
-    build_mean_subject,
-    build_SRM_dataset,
-    create_li2022_datasets,
-)
+from src.li2022_preprocess import build_mean_subject, create_li2022_datasets
 
 create_lebel2023_dataset()
 create_li2022_datasets()
+create_li2022_datasets("FR")
+create_li2022_datasets("CN")
 build_mean_subject(SS=True)
 build_mean_subject(SS=True, trimmed=True)
-build_mean_subject(SS=False)
-build_mean_subject(SS=False, trimmed=True)
-build_SRM_dataset(input_path="datasets/li2022_EN_SS", n_components=500)
-build_SRM_dataset(input_path="datasets/li2022_EN_SS", n_components=2000)
-build_SRM_dataset(input_path="datasets/li2022_EN", n_components=500)
-build_SRM_dataset(input_path="datasets/li2022_EN", n_components=2000)

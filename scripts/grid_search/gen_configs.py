1e5317b3e9c64108757d153311109d0a3fe00b18
Louis Jalouzot <86967472+LouisJalouzot@users.noreply.github.com>
2024-11-29 21:53:00 +0100

    Grid search gen config

--- scripts/grid_search/gen_configs.py
+++ scripts/grid_search/gen_configs.py
@@ -3,15 +3,38 @@ from pathlib import Path
 config_path = Path(__file__).parent / "configs.txt"
 config_path.unlink(missing_ok=True)
 
+
+def get_tags(top_encoding_voxels=None, **kwargs):
+    tags = ["--tags"]
+    if top_encoding_voxels is not None:
+        tags.append(f"'n_voxels={top_encoding_voxels // 1000}k'")
+    # Iterate over kwargs
+    for k, v in kwargs.items():
+        if v is not None:
+            tags.append(f"'{k}={v}'")
+
+    return " ".join(tags)
+
+
+def write_config(*args, **kwargs):
+    args = list(args)
+    for k, v in kwargs.items():
+        if v is not None:
+            if isinstance(v, bool):
+                if v:
+                    args.append(f"--{k}")
+            else:
+                args.append(f"--{k} {v}")
+
+    return " ".join(args) + "\n"
+
+
 n_configs = 0
 default_config = (
-    f"--wandb_project 'fMRI-Decoding-wrap-up' "
-    f'--subjects \'{{"lebel2023": ["UTS01", "UTS02", "UTS03"]}}\' '
-    f"--valid_ratio 0.04 --test_ratio 0.09 --return_tables --cache "
-    f"--top_encoding_voxels 3000 "
-    f"--tags 'Grid search' UTS0123 "
+    "--project_name fMRI-Decoding-grid-search "
+    '--subjects \'{{"lebel2023": ["UTS01", "UTS02", "UTS03"]}}\' '
+    "--top_encoding_voxels 3000 "
 )
-param_names = ["context_length", "lag", "smooth", "stack"]
 params = [
     ("context_length", range(1, 7)),
     ("lag", range(1, 7)),

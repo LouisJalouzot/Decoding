b7dc511864c0d130e8b98e3182d55952bd2f0de0
Louis Jalouzot <86967472+LouisJalouzot@users.noreply.github.com>
2024-12-06 12:51:44 +0100

    Optuna grid search

--- grid_search.py
+++ grid_search.py
@@ -1,32 +1,17 @@
 import hydra
 import numpy as np
-import psutil
 from omegaconf import DictConfig, OmegaConf
 
 from src.decoding import decoding
-from src.utils import console, device
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="grid_search")
-def main(
-    cfg: DictConfig,
-) -> None:
+def main(cfg: DictConfig) -> None:
     cfg = OmegaConf.to_container(cfg, resolve=True)
 
     if cfg["latents_cfg"]["model"] == "llm2vec":
         if not cfg["latents_cfg"]["token_aggregation"] == "mean":
-            console.log(
-                "llm2vec requires token_aggregation to be 'mean'. Skipping this configuration."
-            )
             return np.inf
-
-    num_cpus = psutil.cpu_count()
-    ram = psutil.virtual_memory().total / (1024**3)
-    console.log(
-        f"Number of available CPUs: [green]{num_cpus}[/]\n"
-        f"Available RAM: [green]{ram:.3g} GB[/]\n"
-        f"Using device [green]{device}[/]"
-    )
 
     output = decoding(**cfg)
 

24f5e67501a89f9de820ba6789d36602cf369e50
Louis Jalouzot <86967472+LouisJalouzot@users.noreply.github.com>
2025-01-08 11:05:20 +0100

    Add mean subject creation for LeBel

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@ from src.lebel2023_preprocess import (
     create_lebel2023_balanced_dataset,
     create_lebel2023_dataset,
     create_lebel2023_fmriprep_dataset,
+    create_lebel2023_mean_subject,
 )
 from src.li2022_preprocess import build_mean_subject, create_li2022_datasets
 
@@ -9,6 +10,12 @@ create_lebel2023_dataset()
 create_lebel2023_fmriprep_dataset()
 create_lebel2023_balanced_dataset("lebel2023")
 create_lebel2023_balanced_dataset("lebel2023_fmriprep")
+create_lebel2023_mean_subject("lebel2023_fmriprep", "lebel2023_fmriprep_mean")
+create_lebel2023_mean_subject(
+    "lebel2023_fmriprep",
+    "lebel2023_fmriprep_123_mean",
+    ["UTS01", "UTS02", "UTS03"],
+)
 create_li2022_datasets()
 create_li2022_datasets("FR")
 create_li2022_datasets("CN")

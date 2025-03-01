Fix output dimension calculation in train.py to use test dataset

src/train.py
@@ -220,7 +220,7 @@ def train(
     torch.autograd.set_detect_anomaly(True)
     torch.set_float32_matmul_precision("high")
 
-    out_dim = df_train.Y.iloc[0].shape[1]
+    out_dim = df_test.Y.iloc[0].shape[1]
     decoder = decoder_cfg["class"]
     if hasattr(base_decoders, decoder):
         decoder = getattr(base_decoders, decoder)(